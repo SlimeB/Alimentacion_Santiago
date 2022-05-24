7129f2839dfdb8999450d8e2e314d5b137b0099c
Gabriel Lacunza <gab.lacunza@gmail.com>
2022-05-24 10:13:10 -0400

    detalles paginas

--- main/views.py
+++ main/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    n = range(6)
+    return render(request, 'home.html', {"n": n})
