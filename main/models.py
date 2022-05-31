8b8a6e86a2b1556025fc3d556baf8315ba8ac497
Gabriel Lacunza <gab.lacunza@gmail.com>
2022-05-31 10:07:06 -0400

    haciendolo funcional

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class restaurante(models.Model):
+    id_producto = models.IntegerField(null=False, primary_key=True, unique=True),
+    nombre_restaurante = models.CharField(max_length=50, blank=False)
+
+class producto(models.Model):
+    nombre_producto = models.CharField(max_length=100, blank=False),
+    descripcion_producto = models.CharField(max_length=500, blank=False)
+    restaurante_producto = models.ForeignKey(to=restaurante, on_delete=models.CASCADE)
+    disponible = models.BooleanField(default=False)
