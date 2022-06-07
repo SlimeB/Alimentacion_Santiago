cc4d5fe5c4de1906288468a1e7e487883fefd38e
Gabriel Lacunza <gab.lacunza@gmail.com>
2022-06-07 09:26:23 -0400

    base de datos hecha

--- main/models.py
+++ main/models.py
@@ -9,5 +9,14 @@ class restaurante(models.Model):
 class producto(models.Model):
     nombre_producto = models.CharField(max_length=100, blank=False),
     descripcion_producto = models.CharField(max_length=500, blank=False)
-    restaurante_producto = models.ForeignKey(to=restaurante, on_delete=models.CASCADE)
+    restaurante_producto = models.ForeignKey(to=restaurante,blank=False, on_delete=models.CASCADE)
     disponible = models.BooleanField(default=False)
+
+class categoria_usuario(models.Model):
+    id_categoria = models.IntegerField(primary_key=True, blank= False, unique= True)
+    nombre_categoria = models.CharField(max_length= 80, blank=False, unique=True)
+
+class usuario(models.Model):
+    nombre_usuario = models.CharField(max_length=100, blank=False)
+    contraseña_usuario = models.CharField(max_length=20, blank=False)
+    categoria_usuario = models.ForeignKey(to=categoria_usuario, blank= False, on_delete=models.CASCADE)
